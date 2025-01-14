7e0fa06bacf8e78418d3c1d7c743722645e6a7ea
yahiyasan <141217017+yahiyasan@users.noreply.github.com>
2025-01-14 21:30:41 +0530

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I usually work and play games here.I like Python but simultaneously learing GoLang,Typescript,Rust and some DevOps. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/yahiyasan" target="_blank">Github</a>!`,
+  "cs-degree": `Soon, there will be my CS degree.`,
+  "sofa-table": `That's my sofa. I like to relax here and read books and watch anime.`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `My resume website is under development.`,
+  projects: `Working on multiple projects soon, I will list them here.`,
+  library: `There are a lot of programming books and self help on my shelves.
+  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit Yahiya portfolio, just close the tab.`,
+};
